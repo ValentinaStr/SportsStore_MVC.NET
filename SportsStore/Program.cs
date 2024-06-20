b673484ee19803f95f6adb3253af9d7106bea3ca
Valiantsina <valiantsina_strazhnikava@epam.com>
2024-06-20 14:25:36 +0300

    Add project files.

---
+++ SportsStore/Program.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<StoreDbContext>(opts => {
+    opts.UseSqlServer(
+    builder.Configuration["ConnectionStrings:SportsStoreConnection"]);
+});
+
+var app = builder.Build();
+
+//app.MapGet("/", () => "Hello World!");
+
+app.UseStaticFiles();
+
+app.MapDefaultControllerRoute();
+
+app.Run();
