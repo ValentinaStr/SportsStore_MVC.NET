ecff373252a6c7727e3118653f6d0f75bf7ddce0
Valiantsina <valiantsina_strazhnikava@epam.com>
2024-07-11 12:07:03 +0300

    Added Pagination

---
+++ SportsStore.Tests/PageLinkTagHelperTests.cs
@@ -0,0 +1,110 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Moq;
+using SportsStore.Controllers;
+using SportsStore.Infrastructure;
+using SportsStore.Models;
+using SportsStore.Models.ViewModels;
+using Xunit;
+using static System.Net.Mime.MediaTypeNames;
+namespace SportsStore.Tests
+{
+    public class PageLinkTagHelperTests
+    {
+        [Fact]
+        public void Can_Generate_Page_Links()
+        {
+            // Arrange
+            var urlHelper = new Mock<IUrlHelper>();
+            urlHelper.SetupSequence(x => x.Action(It.IsAny
+           <UrlActionContext>()))
+            .Returns("Test/Page1")
+            .Returns("Test/Page2")
+            .Returns("Test/Page3");
+            var urlHelperFactory = new Mock<IUrlHelperFactory>();
+            urlHelperFactory.Setup(f =>
+            f.GetUrlHelper(It.IsAny<ActionContext>()))
+            .Returns(urlHelper.Object);
+            var viewContext = new Mock<ViewContext>();
+            PageLinkTagHelper helper =
+            new PageLinkTagHelper(urlHelperFactory.Object)
+            {
+                PageModel = new PagingInfo
+                {
+                    CurrentPage = 2,
+                    TotalItems = 28,
+                    ItemsPerPage = 10
+                },
+                ViewContext = viewContext.Object,
+                PageAction = "Test"
+            };
+            TagHelperContext ctx = new TagHelperContext(
+            new TagHelperAttributeList(),
+            new Dictionary<object, object>(), "");
+            var content = new Mock<TagHelperContent>();
+            TagHelperOutput output = new TagHelperOutput("div",
+            new TagHelperAttributeList(),
+            (cache, encoder) => Task.FromResult(content.Object));
+            // Act
+            helper.Process(ctx, output);
+            // Assert
+            Assert.Equal(@"<a href=""Test/Page1"">1</a>"
+            + @"<a href=""Test/Page2"">2</a>"
+            + @"<a href=""Test/Page3"">3</a>",
+            output.Content.GetContent());
+        }
+
+        [Fact]
+        public void Can_Use_Repository()
+        {
+            // Arrange
+            Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[] {
+            new Product {ProductID = 1, Name = "P1"},
+             new Product {ProductID = 2, Name = "P2"}
+            }).AsQueryable<Product>());
+            HomeController controller = new HomeController(mock.Object);
+
+            // Act
+            ProductsListViewModel result =
+            controller.Index()?.ViewData.Model as ProductsListViewModel ?? new();
+
+            // Assert
+            Product[] prodArray = result.Products.ToArray();
+            Assert.True(prodArray.Length == 2);
+            Assert.Equal("P1", prodArray[0].Name);
+            Assert.Equal("P2", prodArray[1].Name);
+        }
+
+        [Fact]
+        public void Can_Paginate()
+        {
+            // Arrange
+            Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[] {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"},
+                new Product {ProductID = 4, Name = "P4"},
+                new Product {ProductID = 5, Name = "P5"}
+                }).AsQueryable<Product>());
+            HomeController controller = new HomeController(mock.Object);
+            controller.PageSize = 3;
+
+            // Act
+            ProductsListViewModel result =
+            controller.Index(2)?.ViewData.Model as ProductsListViewModel ?? new();
+
+            // Assert
+            Product[] prodArray = result.Products.ToArray();
+            Assert.True(prodArray.Length == 2);
+            Assert.Equal("P4", prodArray[0].Name);
+            Assert.Equal("P5", prodArray[1].Name);
+        }
+    }
+}
