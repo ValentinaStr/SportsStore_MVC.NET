9bde1d0c7dc32b47b857b490c6a59d0d3be1004c
Valiantsina <valiantsina_strazhnikava@epam.com>
2024-07-18 17:35:17 +0300

    Synchronized unit tests

--- SportsStore.Tests/PageLinkTagHelperTests.cs
+++ SportsStore.Tests/PageLinkTagHelperTests.cs
@@ -60,8 +60,7 @@ namespace SportsStore.Tests
             HomeController controller = new HomeController(mock.Object);
 
             // Act
-            ProductsListViewModel result =
-            controller.Index()?.ViewData.Model as ProductsListViewModel ?? new();
+            ProductsListViewModel result = controller.Index(null)?.ViewData.Model as ProductsListViewModel ?? new();
 
             // Assert
             Product[] prodArray = result.Products.ToArray();
@@ -86,8 +85,7 @@ namespace SportsStore.Tests
             controller.PageSize = 3;
 
             // Act
-            ProductsListViewModel result =
-            controller.Index(2)?.ViewData.Model as ProductsListViewModel ?? new();
+            ProductsListViewModel result = controller.Index(null, 2)?.ViewData.Model as ProductsListViewModel ?? new();
 
             // Assert
             Product[] prodArray = result.Products.ToArray();
